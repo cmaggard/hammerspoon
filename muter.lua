6b0ce201ee931a51d0531f234adad9b7e1d239a8
Cody Maggard <cody@joinhandshake.com>
2022-05-09 13:18:16 -0400

    Work around Monterrey bug where muting Airpod inputs mutes the whole thing by creating Loopback interface

--- muter.lua
+++ muter.lua
@@ -1,11 +1,16 @@
 hotkey = hs.hotkey.bind({"command", "shift"}, "a", function()
-  local mic = hs.audiodevice.defaultInputDevice()
-  local mutedStatus = mic:inputMuted()
+  -- Hack for systems where I have Loopback installed
+  local mic = hs.audiodevice.findDeviceByName("Microphone Hack")
+  local mic = mic or hs.audiodevice.defaultInputDevice()
+
+  local muted_status = mic:inputMuted()
   local alert_msg
-  if mic:setInputMuted(not mutedStatus) then
-    alert_msg = mutedStatus and "Unmuted" or "Muted"
+
+  if mic:setInputMuted(not muted_status) then
+    alert_msg = muted_status and "Unmuted" or "Muted"
   else
     alert_msg = "Could not toggle mute!"
   end
+
   hs.alert.show(alert_msg)
 end)
