41732fec007fce3fce28a660d159ee471266857b
Cody Maggard <cody@joinhandshake.com>
2022-05-09 12:39:44 -0400

    Explicitly mute input (fixes bug in Monterrey where both input/output would be muted)

--- muter.lua
+++ muter.lua
@@ -1,8 +1,8 @@
 hotkey = hs.hotkey.bind({"command", "shift"}, "a", function()
   local mic = hs.audiodevice.defaultInputDevice()
-  local mutedStatus = mic:muted()
+  local mutedStatus = mic:inputMuted()
   local alert_msg
-  if mic:setMuted(not mutedStatus) then 
+  if mic:setInputMuted(not mutedStatus) then
     alert_msg = mutedStatus and "Unmuted" or "Muted"
   else
     alert_msg = "Could not toggle mute!"
