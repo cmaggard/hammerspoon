ff56050342ad504b0e31766094d7ec1f73b0d19d
Cody Maggard <cody.maggard@gmail.com>
2025-05-28 10:44:03 -0500

    Audio switcher, and display message on primary display

--- muter.lua
+++ muter.lua
@@ -1,16 +1,39 @@
-hotkey = hs.hotkey.bind({"command", "shift"}, "a", function()
-  -- Hack for systems where I have Loopback installed
-  local mic = hs.audiodevice.findDeviceByName("Microphone Hack")
-  local mic = mic or hs.audiodevice.defaultInputDevice()
+local ad = hs.audiodevice
 
-  local muted_status = mic:inputMuted()
-  local alert_msg
+displayMessage = function(msg)
+	hs.alert.show(msg, {}, hs.screen.primaryScreen())
+end
 
-  if mic:setInputMuted(not muted_status) then
-    alert_msg = muted_status and "Unmuted" or "Muted"
-  else
-    alert_msg = "Could not toggle mute!"
-  end
+muter_hotkey = hs.hotkey.bind({ "command", "shift" }, "a", function()
+	-- Hack for systems where I have Loopback installed
+	local mic = ad.findDeviceByName("Microphone Hack")
+	local mic = mic or ad.defaultInputDevice()
 
-  hs.alert.show(alert_msg)
+	local muted_status = mic:inputMuted()
+	local alert_msg
+
+	if mic:setInputMuted(not muted_status) then
+		alert_msg = muted_status and "Unmuted" or "Muted"
+	else
+		alert_msg = "Could not toggle mute!"
+	end
+
+	displayMessage(alert_msg)
 end)
+
+audio_switch_hotkey = hs.hotkey.bind({ "command", "shift" }, "l", function()
+	local headphones = ad.findDeviceByName("External Headphones")
+	local speakers = ad.findDeviceByName("MacBook Pro Speakers")
+	local alert_msg
+
+	if ad.defaultOutputDevice():name() == "External Headphones" then
+		speakers:setDefaultOutputDevice()
+		alert_msg = "Internal Speakers"
+	else
+		headphones:setDefaultOutputDevice()
+		alert_msg = "External Headphones"
+	end
+
+	displayMessage("Switched to " .. alert_msg)
+end)
+
